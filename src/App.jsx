172652008571021201bdc984d4dc7b3003b376f9
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-12-30 02:01:41 +0700

    Add core features: implement authentication context, navigation, and notification system; update styles and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
 import './App.css'
+import './index.css'
+import Navbar from './components/Navbar'
+import { AuthProvider } from './context/AuthContext'
+import { Toaster } from 'sonner'
 
 function App() {
 
   return (
-    <>
-    </>
+    <AuthProvider>
+      <Navbar />
+      <Toaster />
+    </AuthProvider>
   )
 }
 
